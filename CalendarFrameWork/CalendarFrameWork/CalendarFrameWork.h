db84f27bf55e7bd9d88a8d03df51d0895c18d9db
Tonny Franzis <tonny.tblr@gmail.com>
2020-12-02 20:51:24 +0530

    Initial commit

---
+++ CalendarFrameWork/CalendarFrameWork/CalendarFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  CalendarFrameWork.h
+//  CalendarFrameWork
+//
+//  Created by Tblr-Macmini on 02/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CalendarFrameWork.
+FOUNDATION_EXPORT double CalendarFrameWorkVersionNumber;
+
+//! Project version string for CalendarFrameWork.
+FOUNDATION_EXPORT const unsigned char CalendarFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarFrameWork/PublicHeader.h>
+
+
---
+++ CalendarFrameWork/CalendarFrameWork/Service.swift
@@ -0,0 +1,15 @@
+//
+//  Service.swift
+//  CalendarFrameWork
+//
+//  Created by Tblr-Macmini on 02/12/20.
+//
+
+import Foundation
+public class Service{
+    private init(){}
+    public static func doSomething()->String{
+        return "Hi tony your library is working fine"
+    }
+}
+
